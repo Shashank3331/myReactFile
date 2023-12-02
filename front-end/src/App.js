6a3f8a83f5ef8c7f5eb77b226b2886e973572827
Shashank <codewithshash>
2023-12-02 13:32:53 +0530

    mytext

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  const name = "Shashank"
   return (
     <div className='App'>
           <p>Sopify Here...</p>
+          {name}
           <button>Join Now</button>
     </div>
   )
