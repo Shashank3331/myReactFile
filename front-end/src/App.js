1aaec3055f31b14594b81533ccf06ca3ede3cdd8
Shashank <codewithshash>
2023-12-02 13:21:34 +0530

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+          <h2>Sopify Here...</h2>
+          <button>Join Now</button>
+    </div>
+  )
+}
+
+export default App
